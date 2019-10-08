d1b160935a9fcc14b5168e5e76454dddd75c42ab
Oliver <oliver.nilsson94@gmail.com>
2019-10-08 14:06:34 +0200

    Added basic styling and elements for page

--- www/index.js
+++ www/index.js
@@ -1,14 +1,24 @@
 class Index {
   constructor() {
+    this.body = document.querySelector("body")
     this.setUpSections()
     this.getContacts()
   }
 
   setUpSections() {
-    let body = document.querySelector("body")
-    let div = document.createElement("div")
-    div.innerHTML = "Hej! Jag är en div."
-    body.append(div)
+    let header = document.createElement("header")
+    header.setAttribute("class", "header")
+    let title = document.createElement("h1")
+    title.innerHTML = "Kontaktbok"
+    title.setAttribute("class", "title")
+    header.append(title)
+
+    let main = document.createElement("main")
+    main.innerHTML = "Hej! Jag är en main."
+    main.setAttribute("class", "main")
+
+    this.body.append(header)
+    this.body.append(main)
   }
 
   addListeners() {}
